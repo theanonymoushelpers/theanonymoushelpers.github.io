95758621ad4ddf548ce007e4451a7b9a2dab13da
Joel <jocr2024@outlook.com>
2022-11-22 23:36:35 -0800

    website merge with development branch

--- practice/quizlogic.js
+++ practice/quizlogic.js
@@ -1,3 +1,18 @@
+window.PageConfig = {
+    before: (() => {
+      let pagedResolve;
+      let pagedPromise = new Promise((resolve) => {pagedResolve = resolve});
+      
+      MathJax.startup = {
+        ready: () => {
+          MathJax.startup.defaultReady();
+          MathJax.startup.promise.then(() => pagedResolve());
+        }
+      };
+      
+      return () => pagedPromise;
+    })()
+  };
 
 function getQuizParameter(parameterName) {
     let parameters = new URLSearchParams(window.location.search);
@@ -14,7 +29,7 @@ console.log(directQ);
 
 
 //this includes data for quiz length
-var dataQuizLength;
+var dataQuizLength = 7;
 
 //this array stores info of json file
 var dataQA;
@@ -29,6 +44,13 @@ var ansNumber;
 var ansExplain;
 var checkButton = document.getElementById("checkButton")
 var continueButton = document.getElementById("continueButton")
+var continueDiv = document.getElementById("continueDiv")
+var solutionDiv = document.getElementById("solutionDiv")
+var feedbackText = document.getElementById("feedbackText")
+var solutionExpand = document.getElementById("solutionExpand")
+var questionNumber = document.getElementById("questionNumber")
+var solutionText = document.getElementById("sol-text") 
+var titleofQuiz = document.getElementById("quizTitle") 
 
 //speedscore variable
 var speedUpper;
@@ -54,6 +76,33 @@ var ansValue;
 //controls which question is loaded
 var i = 0;
 
+//question number
+var n = 0;
+
+//completed questions for random question loading
+var completedQuestions = [0];
+var checkIfNew = false;
+
+//function picks a random number
+function randomQ() {
+    while (checkIfNew === false) {
+    i = Math.floor(Math.random() * (dataTotalQuestions-1));
+    console.log("i is: " + i);
+         for (let numCheck = 0; numCheck < completedQuestions.length; numCheck++) {
+            if (i === completedQuestions[numCheck]) {
+            checkIfNew = false;
+            break;
+            }
+            else {
+            checkIfNew = true;
+            }
+        }
+    }
+    completedQuestions[n] = i;
+    checkIfNew = false;
+}
+
+
 //right/wrong count
 var corr = 0;
 var inco = 0;
@@ -63,10 +112,15 @@ fetch(directQ)
     .then(response => response.json())
     .then(data => {
         dataQA = data["questionList"]
-        dataQuizLength = data["quCountMinusOne"]
+        dataTotalQuestions = data["totalQuCount"]
+        dataQuizLength = data["quCountToDisplay"]
         //console log validates data is loading correctly for debugging.
         console.log(data)
         console.log(data.quizID)
+        i = Math.floor(Math.random() * (dataTotalQuestions-1));
+        completedQuestions[n] = i;
+        console.log("Questionbank: " + completedQuestions[n]);
+
         questionDisplay.textContent = dataQA[i]["qu"]
         o1.textContent = dataQA[i]["a1"]
         o2.textContent = dataQA[i]["a2"]
@@ -76,8 +130,14 @@ fetch(directQ)
         speedUpper = dataQA[i]["timeUpper"]
         speedMidRange = dataQA[i]["timeMid"]
         speedLower = dataQA[i]["timeLower"]
+        solutionText.textContent = dataQA[i]["solution"]
+        questionNumber.innerHTML = "Question " + (n+1) + " of " + (dataQuizLength);
+        titleofQuiz.innerHTML = data["title"]
         console.log("Speed Upper Req:" + speedUpper)
        } )
+
+
+
 
 
 checkButton.addEventListener('click', checkAnswer);
@@ -85,10 +145,13 @@ continueButton.addEventListener('click', loadNextQuestion);
 
 setInterval(function() {speedTimer += 1; console.log("Tick:" + speedTimer)}, 1000);
 
+
 function checkAnswer() {
     selectedValue = selection.value;
     checkButton.style.visibility = 'hidden';
     continueButton.style.visibility = 'visible';
+    continueDiv.style.visibility = 'visible';
+    solutionDiv.style.visibility = 'visible';
     clearInterval();
     console.log(speedTimer);
 
@@ -97,6 +160,15 @@ function checkAnswer() {
         console.log(ansValue)
         console.log(selectedValue)
         console.log("Speed Upper Req:" + speedUpper)
+
+        continueDiv.style.backgroundColor = '#d5ffd5';
+        solutionDiv.style.backgroundColor = '#d5ffd5';
+        feedbackText.style.color = '#348926';
+        solutionExpand.style.color = '#348926';
+        feedbackText.innerHTML = '&#10004; Great job!';
+        continueButton.classList.remove('buttonStOr')
+        continueButton.classList.add('buttonStGr')
+
         if (speedTimer < speedLower)
         {
             speedPointsCount +=3;
@@ -126,6 +198,14 @@ function checkAnswer() {
     else {console.log("wrong")
         console.log(ansValue)
         console.log(selectedValue)
+        
+        continueDiv.style.backgroundColor = '#ffeed0';
+        solutionDiv.style.backgroundColor = '#ffeed0';
+        feedbackText.style.color = '#8a5d16';
+        solutionExpand.style.color = '#8a5d16';
+        feedbackText.innerHTML = '&#33; Not quite.';
+        continueButton.classList.remove('buttonStGr')
+        continueButton.classList.add('buttonStOr')
         inco++;
         
     }
@@ -133,10 +213,13 @@ function checkAnswer() {
 
 function loadNextQuestion() {
 
-    //if question count is less than 10, load next question, otherwise go to results page with context
-    if (i < dataQuizLength) {
-    i++;
-    console.log(i)
+    //if question count is less than 7, load next question, otherwise go to results page with context
+    if (n < (dataQuizLength-1)) {
+    n++;
+    console.log(n)
+    questionNumber.innerHTML = "Question " + (n+1) + " of " + (dataQuizLength);
+    randomQ();
+    console.log("Questionbank: " + i);
     questionDisplay.textContent = dataQA[i]["qu"]
     o1.textContent = dataQA[i]["a1"]
     o2.textContent = dataQA[i]["a2"]
@@ -146,9 +229,13 @@ function loadNextQuestion() {
     speedUpper = dataQA[i]["timeUpper"]
     speedMidRange = dataQA[i]["timeMid"]
     speedLower = dataQA[i]["timeLower"]
+    solutionText.textContent = dataQA[i]["solution"]
     console.log("Speed Upper Req:" + speedUpper)
     continueButton.style.visibility = 'hidden';
     checkButton.style.visibility = 'visible';
+    continueDiv.style.visibility = 'hidden';
+    solutionDiv.style.visibility = 'hidden';
+    MathJax.typesetPromise();
     speedTimer = 0;
     setInterval(function () {}, 1000);
     }
@@ -161,7 +248,7 @@ function loadNextQuestion() {
             window.location.href = url;
         }
 
-        redirectPage(corr/(dataQuizLength+1)*100 + "%", speedPointsCount/(dataQuizLength+1)*10 + "%", subjectQ, identQ, nameQ)
+        redirectPage(Math.round(corr/(dataQuizLength)*100) + "%", Math.round(speedPointsCount/(dataQuizLength)*10) + "%", subjectQ, identQ, nameQ)
     }
     
 }
